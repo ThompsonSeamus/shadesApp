82872625fb984ef66ecfe4dd7ffc8c50055890fd
seamu.th2363 <seamu.th2363@vusd.us>
2022-09-22 15:28:37 -0700

    I have added a fragment, and started learning

---
+++ app/src/main/java/com/example/shadesapp/ShadesDB.java
@@ -0,0 +1,28 @@
+package com.example.shadesapp;
+
+public class ShadesDB {
+
+    public final String[] shades = {
+            "Red",
+            "Orange",
+            "Yellow",
+            "Gold",
+            "Green",
+            "Light Blue",
+            "Blue",
+            "Indigo",
+            "Violet"
+    };
+
+    public final String[] descriptions = {
+            "Red like blood",
+            "Orange like an orange",
+            "Yellow like a highlighter",
+            "Gold like Jose's watch",
+            "Green like  grass",
+            "Light Blue like blue but brighter",
+            "Blue like the berry",
+            "Indigo like blue but darker",
+            "Violet like the flower"
+    };
+}
